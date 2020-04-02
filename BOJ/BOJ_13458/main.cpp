c4293f6564ecb3a0de37798e084929074bdff051
천정호 <cjsgh85@gmail.com>
2020-04-02 17:19:32 +0900

    BOJ_13458 solution

---
+++ BOJ/BOJ_13458/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+#define endl "\n"
+using namespace std;
+
+int N, B, C;
+vector<int> V;
+long long ans = 0;
+
+void init()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int num;
+        cin >> num;
+        V.push_back(num);
+    }
+    cin >> B >> C;
+}
+
+void solution()
+{
+    // 총감독관이 감독할 수 있는 범위.
+    int i;
+    for (i = 0; i < V.size(); i++)
+    {
+        V[i] -= B;
+        ans++;
+    }
+
+    // 부감독관이 갑독할 수 있는 범위.
+    for (i = 0; i < V.size(); i++)
+    {
+        if (V[i] > 0)
+        {
+            ans += V[i] / C;
+            if (V[i] % C != 0)
+            {
+                ans++;
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    init();
+    solution();
+    cout << ans << endl;
+
+    return 0;
+}
