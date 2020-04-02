9f5aa5f98029732c334a282ea595748c95e42788
천정호 <cjsgh85@gmail.com>
2020-04-02 19:57:00 +0900

    BOJ_11727 solution

---
+++ BOJ/BOJ_11727/main.cpp
@@ -0,0 +1,30 @@
+// 점화식
+// DP[0] = 1, DP[1] = 3
+// DP[i] = DP[i-1] + (DP[i-2] * 2) (i > 2)
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    int DP[1000];
+    DP[0] = 1;
+    DP[1] = 3;
+
+    if (N < 2)
+    {
+        cout << DP[N - 1] << endl;
+        return 0;
+    }
+    for (int i = 2; i < N; i++)
+    {
+        DP[i] = (DP[i - 1] + (DP[i - 2] * 2)) % 10007;
+    }
+    cout << DP[N - 1] << endl;
+
+    return 0;
+}
