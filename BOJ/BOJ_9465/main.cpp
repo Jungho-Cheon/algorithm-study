cfb63d539c33ad50a69cb8c6cd7a3ac926f46183
천정호 <cjsgh85@gmail.com>
2020-04-03 17:13:58 +0900

    BOJ_9465 modified

--- BOJ/BOJ_9465/main.cpp
+++ BOJ/BOJ_9465/main.cpp
@@ -1,6 +1,13 @@
 // https://www.acmicpc.net/problem/9465
 // 스티커
 
+// 상하좌우의 스티커를 고르지 못하는 특징에서 DP의 점화식을 구한다.
+// 배열이 2행의 크기이므로 현재 좌표의 왼쪽 대각선에 위치한 칸과 그 칸의 왼쪽 칸 중 큰값을 더해 나가면 가장 큰 수를 구할 수 있다.
+
+// 점화식
+// DP[i][j] += max(DP[i+1][j-1], DP[i+1][j-2]) (i == 0)
+// DP[i][j] += max(DP[i-1][j-1], DP[i-1][j-2]) (i == 1)
+
 #include <iostream>
 #include <algorithm>
 
@@ -28,10 +35,12 @@ void init()
 void solution()
 {
     int ans = 0;
-    int tmp = 0;
-    int l = 0;
+
+    DP[0][1] += DP[1][0];
+    DP[1][1] += DP[0][0];
+
     // 기준 좌표 (i, j)
-    for (int j = 1; j < N; j++)
+    for (int j = 2; j < N; j++)
     {
         for (int i = 0; i < 2; i++)
         {
